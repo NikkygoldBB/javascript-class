4d15bacd78ebe3874d1b28eb948a756413f60f7d
unknown <kolawoleaderonke@gmail.com>
2025-12-20 17:08:43 +0100

    initial commit

---
+++ tutorial 2/class 4/script.js
@@ -0,0 +1,43 @@
+// single line comments
+
+/* multiple line comments */
+
+// var name= "ronke";// old ways
+//let myName = "ronke"; //new way
+//console.log(myName);
+
+//Arithemetic operations in Javascript (+,-,/,*,%)
+
+//Adittion
+let x ;
+x = 50;
+let y = 50;
+
+// let xType;
+//xType = typeof x;
+//console.log(xType);
+//console.log(typeof y);
+
+total = x + y;
+console.log(total);
+//console.log(x+y);
+
+// subtraction
+
+//remainder = x -y;
+//console.log(remainder); or
+console.log(x - y);
+
+// Division
+console.log(x / y);
+
+// Multiplication
+console.log(x * y)
+
+// Modulo or modulo operator
+let what_remain = x % y;
+console.log("what Remian;", what_remain)
+
+
+
+
---
+++ tutorial 2/tutorial 3/class 5/script.js
@@ -0,0 +1,15 @@
+/*
+var keyboard: it declares a variable, it is the old version introduced in JS and it is globally scoped.
+
+let keyboard: it declares a variable, it is locally scoped
+*/
+
+// {} ===> this is a block of code.
+
+// var age = 30;
+//let age = 30;
+{
+    var age = 50;
+}
+
+console.log(age);
